012eb89670f64128cb608a1babfaf771ccaa61d8
Santiago N. Silva <nehuen.silva.1997@gmail.com>
2025-10-23 19:41:31 -0300

    Dom funciona

--- app.js
+++ app.js
@@ -16,12 +16,16 @@ class Accesorio {
     }
 }
 class Producto {
-    constructor(producto, precio) {
+    constructor(producto, precio, foto) {
         this.producto = producto;
         this.precio = precio;
+        this.foto = foto
     }
     nombre() {
         return this.producto.nombre()
+    }
+    foto() {
+        return this.producto.foto;
     }
 }
 const productos = [
@@ -29,16 +33,46 @@ const productos = [
     new Producto(new Arma("19", "Glock"), 280000),
     new Producto(new Arma("Thunder 9", "Bersa"), 210000),
     new Producto(new Accesorio("Cilindro CO2 x Unidad"), 1500),
-    new Producto(new Accesorio("Balines de PVC x 200g"), 9000)
+    new Producto(new Accesorio("Balines de PVC Blancos x 2000U"), 12000),
+    new Producto(new Accesorio("Balines de PVC Fluorescentes x 2000U"), 12000),
+    new Producto(new Accesorio("Cilindro CO2 x Unidad"), 1500),
+    new Producto(new Accesorio("Balines de PVC Blancos x 2000U"), 12000, "BB2000.png"),
+    new Producto(new Accesorio("Balines de PVC Fluorescentes x 2000U"), 12000, "BL2000.webp")
 ]
-function listarProductos(productos) {
-    let retorno = "Escriba el número del producto para agregarlo al carrito:";
-    let index = 1;
-    for (const item of productos) {
-        retorno = `${retorno}\n${index} - ${item.nombre()}.`;
-        index++;
+function filtrarProductos(productos, tipo) {
+    let filtro;
+    switch (tipo) {
+        case "Armas":
+            filtro = (p) => p.producto instanceof Arma;
+            break;
+        case "Accesorios":
+            filtro = (p) => p.producto instanceof Accesorio;
+            break;
+        default:
+            filtro = (p) => true
     }
-    return `${retorno}\n0 - Terminar compra.`;
+    return productos.filter(filtro);
+}
+function listarProductos(listaProductos, elementId) {
+    let retorno = "";
+    let panel = document.getElementById(elementId);
+    for (const item of listaProductos) {
+        retorno = `${retorno}
+        <div class="cardProducto">
+            <div>
+                <img src="./img/productos/${item.foto}" alt="Foto de ${item.nombre()}">
+            </div>
+            <h4>${item.nombre()}<h4>
+            <h4>$ ${item.precio}</h4>
+            <hr>
+            <div class="bloqueAgregar" data-item=${JSON.stringify(item)}>
+                <button class="minus" type="button">-</button>
+                <span class="counter">0</span>
+                <button class="plus" type="button">+</button>
+            </div>
+        </div>`;
+    }
+    panel.innerHTML = retorno;
 }
 function listarCarrito(carrito) {
     let retorno = "Productos en el carrito:";
@@ -59,6 +93,10 @@ function total(carrito) {
 let carrito = [];
 // alert("Bienvenido a La Reglamentaria Airsoft");
 let precio = 0;
+let filtrados = filtrarProductos(productos, "Armas");
+console.log(filtrados);
+listarProductos(filtrados, "listaProductos");
+// listarProductos(productos, "listaProductos");
 // while (true) {
 //     index = prompt(listarProductos(productos)) - 1;
 //     if (index == -1) {
@@ -75,7 +113,20 @@ let precio = 0;
 let logo = document.getElementById("logoImg");
 logo.addEventListener("mouseover", function () {
     logo.setAttribute("src", "./img/logo_hover.png")
-})
+});
 logo.addEventListener("mouseout", function () {
     logo.setAttribute("src", "./img/logo.png")
-})
+});
+let menu = document.getElementById("menuImg");
+menu.addEventListener("mouseover", function () {
+    menu.setAttribute("src", "./img/menu_hover.png")
+});
+menu.addEventListener("mouseout", function () {
+    menu.setAttribute("src", "./img/menu.png")
+});
+let linkInicio = document.getElementById("linkInicio");
+linkInicio.addEventListener("click", function () {
+
+});
+let linkArmas = document.getElementById("linkArmas");
+let linkAccesorios = document.getElementById("linkAccesorios");
